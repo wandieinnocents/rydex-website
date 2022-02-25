bfe270d81403bdc1430fe2e53a184ea585a0189c
wandieinnocents <wandieinnocent2@gmail.com>
2022-02-25 15:19:40 -0800

    Fix google maps , submit form to mail

--- php/envia_fale.php
+++ php/envia_fale.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 pt-BR > criar email de origem do seu dominio e colocar em EMAIL_MEUDOMINIO@MEUDOMINIO.COM
 en-US >create email from your domain and place it at EMAIL_MEUDOMINIO@MEUDOMINIO.COM
 */ 
-$from = "EMAIL_MEUDOMINIO@MEUDOMINIO.COM";
+$from = "info@rydex.com";
 
 /* 
 pt-BR > colocar o email pelo qual você quer enviar para seu controle é EMAIL_QUE_VAI_ENVIAR@gmail.com.
@@ -20,17 +20,17 @@ $to = "wandieinnocent2@gmail.com";
 pt-BR > agora busca no html pelo 'name' nome do campo, não precisa alterar nada.
 en-US > now search the html for the 'name' field name, don't need to change anything.
 */ 
-$nome = $_POST['nome'];
-$sobrenome = $_POST['sobrenome'];
-$assunto = $_POST['assunto'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$subject = $_POST['subject'];
 $email = $_POST['email'];
-$mensagem = $_POST['mensagem'];
+$message = $_POST['message'];
 
-$corpo = $nome." ".$sobrenome."\n".$assunto."\n".$email."\n".$mensagem;
+$corpo = $first_name." ".$last_name."\n".$subject."\n".$email."\n".$message;
 
 $headers = "De:". $email;
 
-mail($to, $assunto, $corpo, $headers);
+mail($to, $subject, $corpo, $headers);
 
 $var = "<script>javascript:history.back(-1)</script>";
 echo $var;
